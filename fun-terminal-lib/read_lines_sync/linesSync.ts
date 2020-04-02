7f90db35b293ab46b1ad802fb576a7ac22d00541
Cheng <62860928+cheng-code@users.noreply.github.com>
2020-04-01 21:47:52 -0600

    code dump

---
+++ fun-terminal-lib/read_lines_sync/linesSync.ts
@@ -0,0 +1,38 @@
+import { TextProtoReader } from './textprotoSync.ts';
+import { BufReader } from './bufioSync.ts';
+
+/** Yields a buffer of each line given from the reader. */
+export function* linesBuffer(
+	reader: Deno.SyncReader,
+	bufferSize = 4096
+): IterableIterator<Uint8Array> {
+	const tpReader = new TextProtoReader(new BufReader(reader, bufferSize));
+	let buffer: Uint8Array | typeof Deno.EOF | undefined;
+	while (true) {
+		if(buffer == Deno.EOF) {
+			break;
+		}
+		buffer = tpReader.readLineSlice();
+		if(buffer == Deno.EOF) {
+			return;
+		}
+		yield buffer;
+	}
+}
+
+/** Reads from a reader and yields each line as a str. */
+export function* lines(
+	reader: Deno.SyncReader,
+	bufferSize = 4096
+): IterableIterator<string> {
+	const tpReader = new TextProtoReader(new BufReader(reader, bufferSize));
+	let buffer: string | typeof Deno.EOF | undefined;
+	while (true) {
+		if (buffer == Deno.EOF){
+			break;
+		}
+		buffer = tpReader.readLine();
+		if(Deno.EOF === buffer) break;
+		yield buffer;
+	}
+}
