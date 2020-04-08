6bbf7e1b104997d07ef2d569adeb4c4ff60c4427
Cheng <62860928+cheng-code@users.noreply.github.com>
2020-04-07 18:43:51 -0600

    check_myprogram_ts.ts

---
+++ check_programIO_lib/check_myprogram_ts.ts
@@ -0,0 +1,171 @@
+// requires deno
+const Deno_createSync = Deno.createSync;
+const Deno_removeSync = Deno.removeSync;
+const Deno_readdirSync = Deno.readdirSync;
+const Deno_exit = Deno.exit;
+const Deno_openSync = Deno.openSync;
+declare type Deno_Process = Deno.Process;
+const Deno_run = Deno.run;
+const Deno_readFileSync = Deno.readFileSync;
+const Deno_build_os = Deno.build.os;
+
+
+const descendantFilesPaths = (
+    baseDirPath: string[],
+    fileNamesToFind?: string[]
+): string[][] => {
+    const filesDirsPathsHere: string[][] = [];
+    const filesDirsHere = Deno_readdirSync(baseDirPath.join("/"));
+    for (let i: number = 0; i < filesDirsHere.length; ++i) {
+        const fileOrDir = filesDirsHere[i];
+        if (fileOrDir.isDirectory()) {
+            const subDirPath = baseDirPath.concat(
+                fileOrDir.name ? fileOrDir.name : ""
+            );
+            const enclosedFilesDirsPaths: string[][] = descendantFilesPaths(
+                subDirPath,
+                fileNamesToFind
+            );
+            filesDirsPathsHere.push(...enclosedFilesDirsPaths);
+        } else if (fileOrDir.isFile() &&
+            fileOrDir.name &&
+            (!fileNamesToFind ||
+                fileNamesToFind.find((val) => {
+                    return val === fileOrDir.name;
+                }))) {
+            const filePath = baseDirPath.concat(
+                fileOrDir.name ? fileOrDir.name : ""
+            );
+            filesDirsPathsHere.push(filePath);
+        }
+        // not dealing with symlinks
+    }
+    return filesDirsPathsHere;
+};
+
+const descendantFilesPathsStrs = (
+    descendantFilesPathsArr: string[][]
+): string[] => {
+    return descendantFilesPathsArr.reduce((prev, val) => {
+        return prev.concat(val.join("/"));
+    }, []);
+};
+
+
+const diffStudentOutputVsExpected = async (firstMyprogramBaseDirStr: string, inFilename: string, outFilename: string, outExpectedFilename: string) => {
+
+    const txtDec = new TextDecoder("utf-8");
+    const inFile = Deno_openSync(inFilename);
+
+    const studentProgram: Deno_Process = Deno_run(
+        {
+            cmd: ["/bin/sh", "run_program_bash.sh"],
+            cwd: firstMyprogramBaseDirStr,
+            stdout: "piped",
+            stdin: inFile.rid
+        }
+    );
+    let studentProgramOut: string = txtDec.decode(await studentProgram.output());
+    inFile.close();
+
+    // strip out first line if it should be "Using deno"...
+    if (0 === studentProgramOut.indexOf("Using deno")) {
+        const reMatch = /\r?\n/.exec(studentProgramOut);
+        const firstNewlineIdx = reMatch?.index == null ? 0 : reMatch?.index;
+        studentProgramOut = studentProgramOut.substring(firstNewlineIdx + 1);
+    }
+
+    Deno_createSync(outFilename).close();
+    Deno_removeSync(outFilename);
+    const stuProOutFile = Deno_createSync(outFilename);
+    // const stuProOutFile = Deno_openSync(outFilename, { read: true, write: true });
+    stuProOutFile.writeSync(new TextEncoder().encode(studentProgramOut));
+    stuProOutFile.close();
+
+    let diffProcessArg = [
+        "diff",
+        "-Z",
+        "-b",
+        "-d",
+        // "--color=always", // no color on old diff
+        outExpectedFilename,
+        outFilename
+    ];
+    if (Deno_build_os == "win") {
+        diffProcessArg = ["FC", outExpectedFilename, outFilename];
+    }
+
+    const diffPro: Deno_Process = Deno_run(
+        {
+            cmd: diffProcessArg,
+            stdout: "piped"
+        }
+    );
+    const diffRes: string = txtDec.decode(await diffPro.output());
+    if (diffRes == "") {
+        console.log(
+            "Good job!!  No difference from expected output for this example input."
+        );
+    } else {
+        console.log("Unexpected output for this example input:");
+        const outExpected: string = txtDec.decode(Deno_readFileSync(inFilename));
+        console.log(outExpected);
+        console.log("Expected < but got >");
+        console.log(diffRes);
+    }
+}
+
+
+export const main = async () => {
+    const allPaths = descendantFilesPaths(["."], ["myprogram.ts"]);
+    if (allPaths.length <= 0) {
+        console.log("No myprogram.ts found!  Nothing to check.");
+        Deno_exit();
+    }
+
+    const firstMyprogramBaseDir: string[] = allPaths[0];
+    if (firstMyprogramBaseDir.length <= 2) {
+        console.log("myprogram.ts found in: " + firstMyprogramBaseDir.join("/"));
+        console.log(
+            "myprogram.ts is not in proper project folder.\nFix project folder before checking again!"
+        );
+        Deno_exit();
+    }
+
+    firstMyprogramBaseDir.splice(firstMyprogramBaseDir.length - 1, 1);
+    const firstMyprogramBaseDirStr = firstMyprogramBaseDir.join("/");
+    console.log("Checking program in: " + firstMyprogramBaseDirStr);
+
+    const studentProjectRequiredFiles = [
+        "cs10-txt-lib-0.6.ts",
+        "myprogram.ts",
+        "run_program_bash.sh",
+        "run_program_macs.command"
+    ];
+    for (let f of studentProjectRequiredFiles) {
+        try {
+            const requiredFile = Deno_openSync(
+                firstMyprogramBaseDirStr + "/" + f,
+                { read: true }
+            );
+            requiredFile.close();
+        } catch (e) {
+            console.log("Error! Missing file: " + f);
+            Deno_exit();
+        }
+    }
+
+    for (let idx: number = 0; idx < 100; ++idx) {
+        try {
+            const inFilename = "in"+(idx === 0? "" : idx) + ".txt";
+            const outFilename = "out"+(idx === 0? "" : idx) + ".txt";
+            const outExpectedFilename = "outExpected"+(idx === 0? "" : idx) + ".txt";
+            Deno_openSync(inFilename).close();
+            Deno_openSync(outExpectedFilename).close();
+            console.log("\nChecking test case " + idx);
+            await diffStudentOutputVsExpected(firstMyprogramBaseDirStr, inFilename, outFilename, outExpectedFilename);
+        } catch (e){
+            return;
+        }
+    }
+};
