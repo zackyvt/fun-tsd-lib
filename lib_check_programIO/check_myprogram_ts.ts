58923d7c990d891a6db50c0d0dbba16039d9d5ed
Cheng <62860928+cheng-code@users.noreply.github.com>
2020-04-18 04:24:48 -0600

    more refactoring

--- lib_check_programIO/check_myprogram_ts.ts
+++ lib_check_programIO/check_myprogram_ts.ts
@@ -6,57 +6,8 @@ import * as paths_util from "./paths_util.ts";
 import * as checker from "./myprogram_checker_util.ts";
 import * as logging_util from "./logging_util.ts";
 
-const scribe: logging_util.Logger = new logging_util.Logger(true);
-
-const checkMyprogramAtBaseDir = async (myprogramBaseDir: paths_util.path): Promise<string> => {
-    const myprogramBaseDirStr = myprogramBaseDir.join("/");
-    scribe.log("Checking program in: " + myprogramBaseDirStr);
-
-    const folderStructResult = checker.checkStudentProjectFolderStructure(myprogramBaseDir);
-    if (folderStructResult.score !== folderStructResult.maxScore) {
-        scribe.log(folderStructResult.log);
-        return scribe.messagesStr();
-    }
-
-    let correctCount = 0;
-
-    // TODO:  factor out in/outExpected files to be fed in via an array param
-    for (let idx: number = 0; idx < 100; ++idx) {
-        const inFilename = "in" + (idx === 0 ? "" : idx) + ".txt";
-        const outExpectedFilename = "outExpected" + (idx === 0 ? "" : idx) + ".txt";
-        try {
-            Deno_openSync(inFilename).close();
-        } catch (e) {
-            if (idx === 0) {
-                scribe.log("Error: checker is missing " + inFilename);
-            }
-            break;
-        }
-        try {
-            Deno_openSync(outExpectedFilename).close();
-        } catch (e) {
-            if (idx === 0) {
-                scribe.log("Error: checker is missing " + outExpectedFilename);
-            }
-            break;
-        }
-        scribe.log("\nChecking test case " + idx);
-
-        const outFilename = "out" + (idx === 0 ? "" : idx) + ".txt";
-        checker.writeStudentProgramOutputFile(outFilename, checker.postProcessStudentProgramOutput(await checker.runStudentMyProgram(inFilename, myprogramBaseDirStr)));
-        const diffResult: checker.studentProgramAttrQuality = await checker.diffStudentOutputVsExpected(inFilename, outFilename, outExpectedFilename);
-        if (diffResult.score === diffResult.maxScore) {
-            ++correctCount;
-        }
-        scribe.log(diffResult.log);
-    }
-    if (correctCount > 0) {
-        scribe.log("\nRemember just because your program worked ok for " + correctCount + " test " + (correctCount > 1 ? "cases" : "case") + ", it doesn't mean it's perfect!  Your program must work for *all* input as specified in the assigned program specifications!");
-    }
-    return scribe.messagesStr();
-}
-
 export const main = async (): Promise<string> => {
+    const scribe: logging_util.Logger = new logging_util.Logger(true);
     const allPaths = paths_util.relativePathsOfFilesInDir(["."], ["myprogram.ts"]);
     if (allPaths.length <= 0) {
         scribe.log("No myprogram.ts found!  Nothing to check.");
@@ -65,7 +16,12 @@ export const main = async (): Promise<string> => {
 
     const firstMyprogramBaseDir: paths_util.path = allPaths[0];
     firstMyprogramBaseDir.splice(firstMyprogramBaseDir.length - 1, 1);
-    return checkMyprogramAtBaseDir(firstMyprogramBaseDir);
+    return (await checker.checkMyprogramAtBaseDir({
+        myprogramBaseDir: firstMyprogramBaseDir,
+        inOutExpFilePairs: checker.inOutExpectedFilesPaths(["."]),
+        outFileDir: ["."],
+        scribe
+    })).checkerMessageToStudent;
 };
 
 
--- lib_check_programIO/myprogram_checker_util.ts
+++ lib_check_programIO/myprogram_checker_util.ts
@@ -7,32 +7,75 @@ declare type Deno_Process = Deno.Process;
 const Deno_run = Deno.run;
 
 import * as paths_util from "./paths_util.ts";
+import * as logging_util from "./logging_util.ts";
 
 const textDecoderUtf8 = new TextDecoder("utf-8");
 const textEncoder = new TextEncoder();
 
 export type studentProgramAttrQuality = {
     name: string,
-    score: number,
-    maxScore: number,
+    featureCount: number,
+    maxFeatureCount: number,
     log: string
 };
 
+export type inOutExpectedFilesPair = { inFilePath: paths_util.path, outExpectedFilePath: paths_util.path };
+
+export type studentProgramQuality = {
+    myprogramBaseDir: paths_util.path,
+    checkerMessageToStudent: string,
+    inOutExpFilesCount: number,
+    folderStructure: studentProgramAttrQuality,
+    diffResults: studentProgramAttrQuality[],
+    inOutExpNullDiffCount: number
+};
+
+/**
+ * 
+ * @returns array of pairs of in/outExpected paths
+ */
+export const inOutExpectedFilesPaths = (baseDir: paths_util.path): inOutExpectedFilesPair[] => {
+    const paths: inOutExpectedFilesPair[] = [];
+    const maxFilePairsCount: number = 100;
+    for (let idx: number = 0; idx < maxFilePairsCount; ++idx) {
+        const inFilename: string = "in" + (idx === 0 ? "" : idx) + ".txt";
+        const inFilePath: paths_util.path = baseDir.concat(inFilename);
+
+        const outExpectedFilename: string = "outExpected" + (idx === 0 ? "" : idx) + ".txt";
+        const outExpectedFilePath: paths_util.path = baseDir.concat(outExpectedFilename);
+
+        try {
+            const inFileInfo = Deno_openSync(inFilePath.join("/"));
+            try {
+                const outExpFileInfo = Deno_openSync(outExpectedFilePath.join("/"));
+                paths.push({ inFilePath, outExpectedFilePath });
+            } catch (e) {
+                inFileInfo.close();
+                break;
+            }
+        } catch (e) {
+            break;
+        }
+    }
+    return paths;
+}
+
+
 export const checkStudentProjectFolderStructure = (myprogramRelativeBaseDir: paths_util.path): studentProgramAttrQuality => {
-    let ret = {
+    let ret: studentProgramAttrQuality = {
         name: "student project folder structure",
-        score: 0,
-        maxScore: 1,
+        featureCount: 0,
+        maxFeatureCount: 1,
         log: ""
     };
-    if (myprogramRelativeBaseDir[0] !== "."){
+    if (myprogramRelativeBaseDir[0] !== ".") {
         myprogramRelativeBaseDir.splice(0, 0, ".");
     }
     if (myprogramRelativeBaseDir.length < 2) {
         let log = "";
         log += "myprogram.ts found in: " + myprogramRelativeBaseDir.join("/") + "\n";
         log += "myprogram.ts is not in proper project folder.\nFix project folder before checking again!";
-        ret.score = 0;
+        ret.featureCount = 0;
         ret.log = log;
         return ret;
     }
@@ -52,12 +95,12 @@ export const checkStudentProjectFolderStructure = (myprogramRelativeBaseDir: pat
             );
             requiredFile.close();
         } catch (e) {
-            ret.score = 0;
+            ret.featureCount = 0;
             ret.log = "Error! Missing file: " + f;
             return ret;
         }
     }
-    ret.score = ret.maxScore;
+    ret.featureCount = ret.maxFeatureCount;
     ret.log = "";
     return ret;
 };
@@ -85,7 +128,7 @@ export const postProcessStudentProgramOutput = (studentProgramOut: string): stri
 };
 
 
-export const diffStudentOutputVsExpected = async (inFilename: string, outFilename: string, outExpectedFilename: string): Promise<studentProgramAttrQuality> => {
+export const diffStudentVsExpectedOutput = async (inFilename: string, outFilename: string, outExpectedFilename: string): Promise<studentProgramAttrQuality> => {
     const diffProcessArg = [
         "diff",
         // "--color=always", // no color on old diff on Macs
@@ -99,14 +142,14 @@ export const diffStudentOutputVsExpected = async (inFilename: string, outFilenam
     });
     const diffRes: string = textDecoderUtf8.decode(await diffPro.output());
 
-    let ret = {
+    let ret: studentProgramAttrQuality = {
         name: "student program output diff",
-        score: 0,
-        maxScore: 1,
+        featureCount: 0,
+        maxFeatureCount: 1,
         log: ""
     };
     if (diffRes == "") {
-        ret.score = 1;
+        ret.featureCount = 1;
         ret.log = "Good job!!  No difference from expected output for this example input.";
         return ret;
     } else {
@@ -115,7 +158,7 @@ export const diffStudentOutputVsExpected = async (inFilename: string, outFilenam
         log += textDecoderUtf8.decode(Deno_readFileSync(inFilename)) + "\n";
         log += "Expected < but got >" + "\n";
         log += diffRes;
-        ret.score = 0;
+        ret.featureCount = 0;
         ret.log = log;
         return ret;
     }
@@ -134,3 +177,49 @@ export const runStudentMyProgram = async (inFilename: string, myprogramBaseDir: 
     inFile.close();
     return studentProgramOut;
 };
+
+export const checkMyprogramAtBaseDir = async ({ myprogramBaseDir, inOutExpFilePairs, outFileDir, scribe }
+    : { myprogramBaseDir: paths_util.path; inOutExpFilePairs: inOutExpectedFilesPair[]; outFileDir: paths_util.path; scribe: logging_util.Logger })
+    : Promise<studentProgramQuality> => {
+    const myprogramBaseDirStr = myprogramBaseDir.join("/");
+    scribe.log("Checking program in: " + myprogramBaseDirStr);
+
+    const folderStructResult = checkStudentProjectFolderStructure(myprogramBaseDir);
+
+    const ret: studentProgramQuality = {
+        myprogramBaseDir: myprogramBaseDir,
+        checkerMessageToStudent: "",
+        inOutExpFilesCount: inOutExpFilePairs.length,
+        inOutExpNullDiffCount: 0,
+        diffResults: [],
+        folderStructure: folderStructResult
+    }
+
+    if (folderStructResult.featureCount !== folderStructResult.maxFeatureCount) {
+        scribe.log(folderStructResult.log);
+    } else if (inOutExpFilePairs.length === 0) {
+        scribe.log("Error: checker is missing in.txt or outExpected.txt file(s).");
+    } else {
+        let correctCount = 0;
+        for (let idx: number = 0; idx < inOutExpFilePairs.length; ++idx) {
+            const inFilePath: string = inOutExpFilePairs[idx].inFilePath.join("/");
+            const outExpectedFilePath: string = inOutExpFilePairs[idx].outExpectedFilePath.join("/");
+            scribe.log("\nChecking test case " + idx);
+
+            const outFilePath: string = outFileDir.join("/") + "/out" + (idx === 0 ? "" : idx) + ".txt";
+            writeStudentProgramOutputFile(outFilePath, postProcessStudentProgramOutput(await runStudentMyProgram(inFilePath, myprogramBaseDirStr)));
+            const diffResult: studentProgramAttrQuality = await diffStudentVsExpectedOutput(inFilePath, outFilePath, outExpectedFilePath);
+            if (diffResult.featureCount === diffResult.maxFeatureCount) {
+                ++correctCount;
+            }
+            scribe.log(diffResult.log);
+            ret.diffResults[idx] = diffResult;
+        }
+        ret.inOutExpNullDiffCount = correctCount;
+        if (correctCount > 0) {
+            scribe.log("\nRemember just because your program worked ok for " + correctCount + " test " + (correctCount > 1 ? "cases" : "case") + ", it doesn't mean it's perfect!  Your program must work for *all* input as specified in the assigned program specifications!");
+        }
+    }
+    ret.checkerMessageToStudent = scribe.messagesStr();
+    return ret;
+}
