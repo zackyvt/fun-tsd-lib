a7c12ce1dfff027ca8883b42c6286c27d7e273ad
Cheng <62860928+cheng-code@users.noreply.github.com>
2022-05-26 12:43:36 -0600

    fix: check if animation boolean flipped

--- lib-gqguardrail/src/gqg-mod.ts
+++ lib-gqguardrail/src/gqg-mod.ts
@@ -396,6 +396,7 @@ export const createSpriteInGroup: CreateSpriteInGroupFn = function (
         } else if (arguments.length === 3) {
             if (typeof arguments[2] !== "object") {
                 throwConsoleErrorInMyprogram("Third argument for createSpriteInGroup expected to be a dictionary. Instead found: " + arguments[2]);
+            } else if (theAnimation instanceof Object && "imageURL" in theAnimation && typeof theAnimation["imageURL"] === "string") {
                 throwConsoleErrorInMyprogram("Third argument for createSpriteInGroup expected to be a dictionary. Instead found this animation: " + theAnimation + " with imageURL: " + theAnimation["imageURL"] + ". Maybe wrong number of arguments provided? Check API documentation for details of parameters.");
             } // else hope it's a proper standard options map
         } else {
